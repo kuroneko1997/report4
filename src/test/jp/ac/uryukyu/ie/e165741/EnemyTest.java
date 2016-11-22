6f0d1c7c3dae6cbf2f547728e80e2d7eb285e3c0
toguchi takeryu <e165741@ie.u-ryukyu.ac.jp>
2016-11-22 20:43:35 +0900

    プログラミング２課題・レポート４

--- src/test/jp/ac/uryukyu/ie/e165741/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165741/EnemyTest.java
@@ -14,11 +14,10 @@ public class EnemyTest {
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.dead = true;
-        for(int i=0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
         assertEquals(heroHP, hero.hitPoint);
-
     }
 
 }
